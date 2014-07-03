4439f2743296715ed0e8eecf50aa46d749b167db
Tyler Geery <tyler.geery@gmail.com>
2014-07-03 19:01:29 -0400

    Added helper methods for specs

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -4,13 +4,6 @@ describe "Adding todo items" do
 
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{todo_list.id}" do
-			click_link "List Items"
-		end
-	end
-
 	it "is successful with valid content" do
 
 		visit_todo_list(todo_list)
