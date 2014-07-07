fb75f044aca095e54eca9ffe8269a76d480de0fe
Tyler Geery <tyler.geery@gmail.com>
2014-07-07 01:12:16 -0400

    Added completed option for todo items w/ specs

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -47,8 +47,9 @@ class TodoItemsController < ApplicationController
 
   def complete
     @todo_item = @todo_list.todo_items.find(params[:id])
-    @todo_item.update_attributes(:completed_at, Time.now)
-    redirect_to todo_list_todo_items_path notice: "Todo item marked as complete"
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
+  end
 
   def url_options
   	{ todo_list_id: params[:todo_list_id] }.merge(super)
